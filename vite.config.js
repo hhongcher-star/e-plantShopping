403be254fc96ef7acf32e1c85e2a4e06d502b984
lxr lexis <hhongcher@gmail.com>
2026-03-03 14:15:05 +0800

    vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
+  base: '/e-plantShopping/',   // ⚠ 必须和仓库名完全一样
   plugins: [react()],
 })
