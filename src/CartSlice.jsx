f1774b12f0d5665bc61888edb693c5fed33513e4
lxr lexis <hhongcher@gmail.com>
2026-03-03 13:33:26 +0800

    Complete Redux cart reducers

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,11 +7,21 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
+      const newitem = action.payload;
+      state.items.push(newitem);
+      console.log(state.items);
     
     },
     removeItem: (state, action) => {
+      const itemToRemove=action.payload
+      state.items=state.items.filter(item=>item.name!==itemToRemove.name)
+      console.log(state.items)
     },
     updateQuantity: (state, action) => {
+      const {name,quantity}=action.payload
+      const itemToUpdate=state.items.find(item=>item.name===name)
+      itemToUpdate.quantity=quantity
+      console.log(state.items)
 
     
     },
